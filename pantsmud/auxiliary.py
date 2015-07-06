3c64c9bfb46a22229bf9de55d953653655db9fbf
Evan Davis <me@ezdwt.com>
2015-07-05 18:33:02 -0700

    Added docstrings to pantsmud.auxiliary

--- pantsmud/auxiliary.py
+++ pantsmud/auxiliary.py
@@ -1,3 +1,42 @@
+"""
+A plugin implementation that allows developers to add custom data/behaviour to the core game objects without having to
+modify their code.
+
+Example of defining auxiliary data:
+
+    class MyAux(object):
+        def __init__(self):
+            self.foo = "bar"
+
+        def load_data(self, data):
+            self.foo = data["bar"]
+
+        def save_data(self):
+            return {"foo": self.foo}
+
+        def do_something(self):
+            print self.foo
+
+    auxiliary.install("baz", "my_aux", MyAux)
+
+Example of enabling auxiliary data on a custom game object:
+
+    class MyGameObject(object):
+        def __init__(self):
+            self.aux = auxiliary.new_data("baz")
+
+        def load_data(self, data):
+            self.aux = auxiliary.load_data(self.aux, data["auxiliary"]
+
+        def save_data(self):
+            return {"auxiliary": auxiliary.save_data(self.aux)}
+
+Example of using the auxiliary data defined on the custom game object:
+
+    def func(game_object):
+        game_object.aux["my_aux"].do_something()
+"""
+
 import logging
 
 log = logging.getLogger(__name__)
@@ -23,6 +62,9 @@ _auxiliary_classes = {
 
 
 def install(aux_type, name, cls):
+    """
+    Add a new auxiliary data class to the given aux_type.
+    """
     if aux_type not in _auxiliary_classes:
         log.debug("Adding new auxiliary type: '%s'", aux_type)
         _auxiliary_classes[aux_type] = {}
@@ -32,6 +74,9 @@ def install(aux_type, name, cls):
 
 
 def new_data(aux_type):
+    """
+    Returns a dictionary containing instances of all aux data for the given aux_type.
+    """
     data = {}
     if aux_type not in _auxiliary_classes:
         return data
@@ -41,6 +86,15 @@ def new_data(aux_type):
 
 
 def load_data(aux, data):
+    """
+    Loads a dictionary containing saved aux data onto the object.
+
+    Data layout:
+        {
+            "my_aux_1": <dict>,  # This will be passed to aux["my_aux_1"].load_data
+            "my_aux_2": <dict>  # This will be passed to aux["my_aux_2"].load_data
+        }
+    """
     aux_keys = set(aux.keys())
     data_keys = set(data.keys())
     keys = aux_keys.intersection(data_keys)
@@ -57,6 +111,9 @@ def load_data(aux, data):
 
 
 def save_data(aux):
+    """
+    Returns a dictionary containing aux data ready to be serialized.
+    """
     data = {}
     for key in aux.keys():
         data[key] = aux[key].save_data()
