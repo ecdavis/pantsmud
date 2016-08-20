881fd71d6b4efc11a802b4f1c9736e9816a181d1
Evan Davis <me@ezdwt.com>
2016-08-19 23:34:09 -0700

    Added setup.py.

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name="pantsmud",
+    version="0.1",
+    description="A simple MUD library built using the Pants networking framework.",
+    author="ecdavis",
+    author_email="me@ezdwt.com",
+    url="http://github.com/ecdavis/pantsmud",
+    download_url="https://github.com/ecdavis/pantsmud/tarball/master",
+    packages=["pantsmud", "pantsmud.driver"],
+    install_requires=["pants >= 1.0"],
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Environment :: Other Environment",
+        "Intended Audience :: Developers",
+        "Natural Language :: English"
+    ]
+)
